dc89341e9a1ed937e5a82598ef2ded3d7a3c124b
WinPhyoEi <winphyoei113@gmail.com>
2020-09-23 23:55:53 +0630

    Updates

---
+++ precache-manifest.175098f40ebc0d4925061c74a2c51051.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740a2aedb049bb113c3bbfb492e45d2d",
+    "url": "/CounterNew/index.html"
+  },
+  {
+    "revision": "00ce0bcc5a4003e488c7",
+    "url": "/CounterNew/static/css/2.a687caca.chunk.css"
+  },
+  {
+    "revision": "50393282388db4c6a045",
+    "url": "/CounterNew/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "00ce0bcc5a4003e488c7",
+    "url": "/CounterNew/static/js/2.c1eb85ae.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/CounterNew/static/js/2.c1eb85ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50393282388db4c6a045",
+    "url": "/CounterNew/static/js/main.703e6e9b.chunk.js"
+  },
+  {
+    "revision": "180a9e12ca8b67e01734",
+    "url": "/CounterNew/static/js/runtime-main.bb9787d9.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/CounterNew/static/media/logo.ee7cd8ed.svg"
+  }
+]);
